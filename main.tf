2305cac9dd8ae1c6c690c26fa806362d7689d27f
Inception <62402529+maksym-butusov@users.noreply.github.com>
2021-11-26 13:48:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,11 @@ provider "aws" {
 # }
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "echo test"
+    command = <<-EOT
+       curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
+       unzip awscliv2.zip
+       ./aws/install
+       aws --version
+    EOT
   }
 }
