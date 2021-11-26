59c6d9d538f2f94f27e608a76cdb2c211a31a976
Inception <62402529+maksym-butusov@users.noreply.github.com>
2021-11-26 13:32:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,29 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "ubuntu" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  availability_zone           = "${var.aws_region}b"
-  associate_public_ip_address = "true"
-  tags = {
-    owner = "email@example.com"
-  }
-}
+# resource "aws_instance" "ubuntu" {
+#   ami                         = var.ami_id
+#   instance_type               = var.instance_type
+#   availability_zone           = "${var.aws_region}b"
+#   associate_public_ip_address = "true"
+#   tags = {
+#     owner = "email@example.com"
+#   }
+# }
 
-resource "aws_s3_bucket" "Devops" {
-  bucket = "devops-44439483843894839"
-  acl    = "private"
+# resource "aws_s3_bucket" "Devops" {
+#   bucket = "devops-44439483843894839"
+#   acl    = "private"
 
-  tags = {
-    Environment = "Dev"
-    owner = "email@example.com"
+#   tags = {
+#     Environment = "Dev"
+#     owner = "email@example.com"
+#   }
+# }
+resource "null_resource" "run" {
+
+ provisioner "local-exec" {
+
+    command = "/bin/bash aws --version"
   }
 }
