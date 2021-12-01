6214a880fd64dd7a0d428c14376f6a1f0864f2b5
Inception <62402529+maksym-butusov@users.noreply.github.com>
2021-12-01 19:47:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = var.aws_region
-}
+# provider "aws" {
+#   region = var.aws_region
+# }
 
 # resource "aws_instance" "ubuntu" {
 #   ami                         = var.ami_id
@@ -21,14 +21,14 @@ provider "aws" {
 #     owner = "email@example.com"
 #   }
 # }
-resource "null_resource" "example35454" {
-  provisioner "local-exec" {
-    command = <<-EOT
-       aws --version
-       aws sts get-caller-identity
-    EOT
-  }
-}
+# resource "null_resource" "example35454" {
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#        aws --version
+#        aws sts get-caller-identity
+#     EOT
+#   }
+# }
 
 # resource "null_resource" "example2" {
 #   provisioner "local-exec" {
@@ -42,3 +42,34 @@ resource "null_resource" "example35454" {
 #     EOT
 #   }
 # }
+resource "aws_cloudformation_stack" "network" {
+  name = "test-vpc-for-importing"
+
+  parameters = {
+    VPCCidr = "192.168.0.0/27"
+  }
+
+  template_body = <<STACK
+{
+  "Parameters" : {
+    "VPCCidr" : {
+      "Type" : "String",
+      "Default" : "192.168.0.0/27",
+      "Description" : "Enter the CIDR block for the VPC. Default is 192.168.0.0/27."
+    }
+  },
+  "Resources" : {
+    "myVpc": {
+      "Type" : "AWS::EC2::VPC",
+      "Properties" : {
+        "CidrBlock" : { "Ref" : "VPCCidr" },
+        "Tags" : [
+          {"Key": "Name", "Value": "Primary_CF_VPC"}
+        ]
+      }
+    }
+  }
+}
+STACK
+}
+
